ea05126457046ad1cba2135b29332325e4f2362d
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-03-20 18:22:42 -0300

    YE-3 refactor: format

--- components/GenericButton.tsx
+++ components/GenericButton.tsx
@@ -2,31 +2,31 @@ import { StyleSheet, Pressable, DimensionValue } from "react-native";
 import { Text } from "./Themed";
 
 
-export function GenericButton(props:{title: string, color?: string, onPress: Function, fontColor?:string, height?: DimensionValue, width?: DimensionValue}){
+export function GenericButton(props: { title: string, color?: string, onPress: Function, fontColor?: string, height?: DimensionValue, width?: DimensionValue }) {
     const styles = StyleSheet.create({
-        container:{
+        container: {
             borderColor: 'rgba(229, 231, 235, 1)',
             borderWidth: 1,
             borderRadius: 100,
             backgroundColor: props.color ?? '#407CE2',
             justifyContent: 'center',
             alignItems: 'center',
-            height: props.height ?? 'auto',      
+            height: props.height ?? 'auto',
             width: props.width ?? '80%'
         },
-        text:{
+        text: {
             color: props.fontColor ?? "#fff",
             fontWeight: '600',
             fontSize: 18,
         }
     })
 
-    return(
+    return (
         <Pressable style={styles.container} onPress={() => props.onPress()}>
             <Text style={styles.text}>
                 {props.title}
             </Text>
         </Pressable>
     )
-    
+
 }
