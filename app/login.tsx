b33bc0cec5d2e627bb398b9ed9aac2b91ece75d8
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-03-20 23:27:37 -0300

    YE-2 move login function to utils

--- app/login.tsx
+++ app/login.tsx
@@ -6,18 +6,9 @@ import { GenericButton } from '@/components/GenericButton';
 import { Email, Password, EyeSlash, Eye, Google, Facebook, Heartbeat } from "@/assets/images/index";
 import { useState } from 'react';
 import { GenericIconButton } from '@/components/GenericIconButton';
-import { FacebookAuthProvider, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/firebase/index";
-import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
-import { signInWithCredential } from 'firebase/auth';
-import { AccessToken, LoginManager } from "react-native-fbsdk-next";
-import { UserProvider, useUser } from "../contexts/user";
-import { ActivityIndicator } from 'react-native';
-
-GoogleSignin.configure({
-  webClientId: "147160860966-am6ip3ii0mro78t0rld4rrp3gmufrcqa.apps.googleusercontent.com"
-});
-
+import { UserProvider, useUser } from "@/contexts/user";
+import { ActivityIndicator } from 'react-native-paper';
+import { handleLoginMethods } from '@/utils/auth';
 
 
 
@@ -26,117 +17,13 @@ export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleLogin = () => {
-    const emailRegex: RegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!emailRegex.test(email)) {
-      alert("Email inválido")
-      return;
-    }
-    if (password.length < 6) {
-      alert("Senha deve ter no mínimo 6 caracteres")
-      return;
-    }
-    setIsLoading(true);
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        setUser(user)
-        router.navigate("/home")
-        setIsLoading(false);
-        return user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(
-          `Erro ao fazer login: ${errorCode} - ${errorMessage}`
-        )
-        setIsLoading(false);
-        return;
-      });
-  };
-
-
-  const handleGoogleLogin = async () => {
-    try {
-      setIsLoading(true);
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      const googleCredential = GoogleAuthProvider.credential(userInfo.idToken);
-      const user = await signInWithCredential(auth, googleCredential);
-      setUser(user.user)
-      router.navigate("/home")
-      setIsLoading(false);
-      return user;
-    } catch (error) {
-      if (error instanceof Error && "code" in error) {
-        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-          // user cancelled the login flow
-          alert("Google login cancelled")
-        } else if (error.code === statusCodes.IN_PROGRESS) {
-          // operation (e.g. sign in) is in progress already
-          alert("Google login in progress")
-        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-          // play services not available or outdated
-          alert("Google login play services not available")
-        } else {
-          // some other error happened
-          alert(error)
-        }
-        setIsLoading(false);
-        return;
-      }
-    }
-  }
-
-  const handleVagabundoLogin = async () => {
-    router.navigate("home")
-  }
-
-
-  const handleFacebookLogin = async () => {
-    setIsLoading(true);
-    // Attempt a login using the Facebook login dialog asking for default permissions.
-    await LoginManager.logInWithPermissions(["public_profile"]).then(
-      async function (result) {
-        if (result.isCancelled) {
-          alert("Login cancelled");
-          return;
-        } else {
-          alert(
-            "Login success with permissions: " +
-            result.grantedPermissions?.toString()
-          );
-          const accessTokenResponse = await AccessToken.getCurrentAccessToken();
-          alert(accessTokenResponse?.accessToken);
-          if (accessTokenResponse === null) {
-            alert("No access token");
-            setIsLoading(false);
-            return;
-          };
-          const facebookCredential = FacebookAuthProvider.credential(accessTokenResponse.accessToken);
-          const user = await signInWithCredential(auth, facebookCredential);
-          setUser(user.user)
-          LoginManager.logOut();
-          router.navigate("/home")
-          setIsLoading(false);
-          return result;
-        }
-      },
-      function (error) {
-        setIsLoading(false);
-        alert("Login fail with error: " + error);
-        return;
-      }
-    );
-  };
-
   const [showPassword, setShowPassword] = useState(false);
-
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
+
+
+  const { handleLogin, handleGoogleLogin, handleFacebookLogin, handleVagabundoLogin } = handleLoginMethods(email, password, setIsLoading, setUser);
   return (
     <UserProvider>
       <View style={styles.container}>
@@ -159,7 +46,7 @@ export default function Home() {
           <View style={styles.extraOptions}>
             <GenericIconButton onPress={handleGoogleLogin} text="Entrar com o Google" ImageComponent={Google}></GenericIconButton>
             <GenericIconButton onPress={handleFacebookLogin} text="Entrar com o Facebook" ImageComponent={Facebook}></GenericIconButton>
-            <GenericIconButton onPress={handleVagabundoLogin} text="Sou Vagabundo" ImageComponent={Heartbeat}></GenericIconButton>
+            <GenericIconButton onPress={handleVagabundoLogin} text="TESTE" ImageComponent={Heartbeat}></GenericIconButton>
           </View>
         </View>
 
