f1ad6755a4d39d0a426a050461d1e7c3842a21e5
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-03-20 23:26:29 -0300

    YE-2 feat(user): set up user document if it doesn't exist

--- contexts/user.tsx
+++ contexts/user.tsx
@@ -1,6 +1,8 @@
 // UserContext.tsx
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { User } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '@/firebase';
 
 interface UserContextProps {
     user: User | null;
@@ -11,6 +13,22 @@ export const UserContext = createContext<UserContextProps | undefined>(undefined
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const checkUserDocument = async (user: User | null) => {
+            if (user) {
+                console.log('user exists');
+                const userDocRef = doc(db, 'users', user.uid);
+                const userDocSnap = await getDoc(userDocRef);
+                if (!userDocSnap.exists()) {
+                    console.log('user document does not exist');
+                    await setDoc(userDocRef, { id: user.uid, email: user.email, displayName: user.displayName, photoURL: user.photoURL, createdAt: new Date(), updatedAt: new Date(), role: 'user', status: 'active', lastLogin: new Date(), lastLogout: new Date(), lastActivity: new Date(), lastPasswordChange: new Date(), lastEmailChange: new Date(), lastProfileUpdate: new Date(), emailVerified: user.emailVerified, phoneNumber: user.phoneNumber, providerId: user.providerId, isAnonymous: user.isAnonymous, tenantId: user.tenantId, });
+                }
+            }
+        };
+
+        checkUserDocument(user);
+    }, [user]);
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
