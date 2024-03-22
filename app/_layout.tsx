12233dc568bf2cb328b22942db143d4e81088ec4
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-03-22 20:11:06 -0300

    YE-31 feat(dark-mode): change provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,11 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import { ThemeProvider } from '@/contexts/theme';
 import { UserProvider } from '../contexts/user';
 import { Provider } from 'react-native-paper';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -46,10 +43,8 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider>
       <Provider>
         <UserProvider>
           <Stack screenOptions={{ headerShown: false }}>
