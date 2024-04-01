dd4a5a2b428b4cb3057e92b79d725c31b0b87a41
EnricoGia <rico500000@gmail.com>
2024-04-01 19:53:15 -0300

    YE-14 feature(style): adjusting styling on appointments create and edit view

--- components/AppointmentForm.tsx
+++ components/AppointmentForm.tsx
@@ -4,6 +4,10 @@ import { useAppointments } from "@/hooks/appointment";
 import { router } from "expo-router";
 import { TextInput } from "react-native-paper";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { StyleSheet } from "react-native";
+import Colors from "@/constants/Colors";
+import { useTheme } from "@/contexts/theme";
+import { RFValue } from "react-native-responsive-fontsize";
 
 type Appointment = {
     appointmentSlug: string;
@@ -15,6 +19,8 @@ const AppointmentForm = (appointment: Appointment) => {
     const [description, setDescription] = useState("");
     const [datetime, setDatetime] = useState(new Date());
     const { createAppointment, editAppointment } = useAppointments();
+    const { theme } = useTheme();
+
     const handleSave = () => {
         if (appointmentSlug !== "new") {
             editAppointment(appointmentSlug, {
@@ -33,28 +39,57 @@ const AppointmentForm = (appointment: Appointment) => {
         router.push("/appointments");
     };
 
+    const styles = StyleSheet.create({
+        container: {
+            width: "80%",
+            top: "20%"
+        },
+        title: {
+            fontWeight: "bold",
+            fontSize: RFValue(20, 808),
+            alignSelf: "center",
+        },
+        input: {
+            backgroundColor: Colors[theme].background,
+            borderColor: Colors[theme].borderColor,
+            borderWidth: RFValue(1, 808),
+        },
+        forms: {
+            top: RFValue(20, 808),
+        },
+    });
     return (
-         <View>
-            <Text>
-                {appointmentSlug !=="new" ? "Edit Appointment" : "Create Appointment"}
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                {appointmentSlug !== "new"
+                    ? "Editar consulta"
+                    : "Criar consulta"}
             </Text>
-            <TextInput
-                label="Nome do doutor"
-                placeholder="Nome do doutor"
-                onChange={(e) => setDoctor(e.nativeEvent.text)}
-            ></TextInput>
-            <TextInput
-                label="Descrição da consulta"
-                placeholder="Descrição da consulta"
-                onChange={(e) => setDescription(e.nativeEvent.text)}
-            ></TextInput>
-            {/* <DateTimePicker
+            <View style={styles.forms}>
+                <TextInput
+                    label="Nome do doutor"
+                    placeholder="Nome do doutor"
+                    onChange={(e) => setDoctor(e.nativeEvent.text)}
+                    style={styles.input}
+                ></TextInput>
+                <TextInput
+                    label="Descrição da consulta"
+                    placeholder="Descrição da consulta"
+                    onChange={(e) => setDescription(e.nativeEvent.text)}
+                    style={styles.input}
+                ></TextInput>
+                {/* <DateTimePicker
                 value={new Date()}
                 onChange={(e) => {
                     setDatetime(new Date(e.nativeEvent.timestamp!));
                 }}
             /> */}
-            <Button title="Save" onPress={handleSave} />
+                <Button
+                    color={Colors[theme].altTextColor}
+                    title="Salvar"
+                    onPress={handleSave}
+                />
+            </View>
         </View>
     );
 };
