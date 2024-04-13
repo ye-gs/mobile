e410793ccc1c415366930f7b2d95001942dea3dd
EnricoGia <rico500000@gmail.com>
2024-04-13 19:01:35 -0300

    feature(types): font weight type

--- types/declaration.d.ts
+++ types/declaration.d.ts
@@ -4,3 +4,17 @@ declare module "*.svg" {
     const content: React.FC<SvgProps>;
     export default content;
 }
+
+
+type WeightValue = 'normal'
+| 'bold'
+| '100'
+| '200'
+| '300'
+| '400'
+| '500'
+| '600'
+| '700'
+| '800'
+| '900'
+| undefined;
