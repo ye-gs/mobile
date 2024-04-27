9940eb3032e310f3df28a096de727c98401fae05
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-04-27 17:24:54 -0300

    fix: children tabs now have themed headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,10 @@ import { useFonts } from "expo-font";
 import { Link, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import { ThemeProvider } from "@/contexts/theme";
+import { ThemeProvider, useTheme } from "@/contexts/theme";
 import { UserProvider } from "../contexts/user";
 import { Provider } from "react-native-paper";
+import Colors from "@/constants/Colors";
 export {
     // Catch any errors thrown by the Layout component.
     ErrorBoundary,
@@ -39,87 +40,109 @@ export default function RootLayout() {
         return null;
     }
 
-    return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
     return (
         <ThemeProvider>
             <Provider>
                 <UserProvider>
-                    <Stack screenOptions={{ headerShown: false }}>
-                        <Stack.Screen name="index" />
-                        <Stack.Screen name="login" />
-                        <Stack.Screen name="signup" />
-                        <Stack.Screen
-                            name="userinfo"
-                            options={{
-                                headerLeft: () => (
-                                    <Link href="/profile">
-                                        <FontAwesome
-                                            size={38}
-                                            name="arrow-left"
-                                        />
-                                    </Link>
-                                ),
-
-                                headerShown: true,
-                                headerTitle: "Suas Informações",
-                                headerTitleAlign: "center",
-                            }}
-                        />
-                        <Stack.Screen
-                            name="settings"
-                            options={{
-                                headerLeft: () => (
-                                    <Link href="/profile">
-                                        <FontAwesome
-                                            size={38}
-                                            name="arrow-left"
-                                        />
-                                    </Link>
-                                ),
-                                headerShown: true,
-                                headerTitle: "Configurações",
-                                headerTitleAlign: "center",
-                            }}
-                        />
-                        <Stack.Screen
-                            name="about"
-                            options={{
-                                headerLeft: () => (
-                                    <Link href="/profile">
-                                        <FontAwesome
-                                            size={38}
-                                            name="arrow-left"
-                                        />
-                                    </Link>
-                                ),
-                                headerShown: true,
-                                headerTitle: "Sobre nós",
-                                headerTitleAlign: "center",
-                            }}
-                        />
-                        <Stack.Screen name="(tabs)" />
-                        <Stack.Screen
-                            name="appointments/[slug]"
-                            options={{
-                                headerLeft: () => (
-                                    <Link href="/appointments">
-                                        <FontAwesome
-                                            size={38}
-                                            name="arrow-left"
-                                        />
-                                    </Link>
-                                ),
-                                headerShown: true,
-                                headerTitle: "Detalhes da Consulta",
-                                headerTitleAlign: "center",
-                            }}
-                        />
-                    </Stack>
+                    <RootLayoutNav />
                 </UserProvider>
             </Provider>
         </ThemeProvider>
+    );
+}
+
+function RootLayoutNav() {
+    const { theme } = useTheme();
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="login" />
+            <Stack.Screen name="signup" />
+            <Stack.Screen
+                name="userinfo"
+                options={{
+                    headerLeft: () => (
+                        <Link href="/profile">
+                            <FontAwesome
+                                size={38}
+                                name="arrow-left"
+                                color={Colors[theme].text}
+                            />
+                        </Link>
+                    ),
+                    headerShown: true,
+                    headerTitle: "Suas Informações",
+                    headerTitleAlign: "center",
+                    headerStyle: {
+                        backgroundColor: Colors[theme].background,
+                    },
+                    headerTitleStyle: { color: Colors[theme].text },
+                }}
+            />
+            <Stack.Screen
+                name="settings"
+                options={{
+                    headerLeft: () => (
+                        <Link href="/profile">
+                            <FontAwesome
+                                size={38}
+                                name="arrow-left"
+                                color={Colors[theme].text}
+                            />
+                        </Link>
+                    ),
+                    headerShown: true,
+                    headerTitle: "Configurações",
+                    headerTitleAlign: "center",
+                    headerStyle: {
+                        backgroundColor: Colors[theme].background,
+                    },
+                    headerTitleStyle: { color: Colors[theme].text },
+                }}
+            />
+            <Stack.Screen
+                name="about"
+                options={{
+                    headerLeft: () => (
+                        <Link href="/profile">
+                            <FontAwesome
+                                size={38}
+                                name="arrow-left"
+                                color={Colors[theme].text}
+                            />
+                        </Link>
+                    ),
+                    headerShown: true,
+                    headerTitle: "Sobre nós",
+                    headerTitleAlign: "center",
+                    headerStyle: {
+                        backgroundColor: Colors[theme].background,
+                    },
+                    headerTitleStyle: { color: Colors[theme].text },
+                }}
+            />
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen
+                name="appointments/[slug]"
+                options={{
+                    headerLeft: () => (
+                        <Link href="/appointments">
+                            <FontAwesome
+                                size={38}
+                                name="arrow-left"
+                                color={Colors[theme].text}
+                            />
+                        </Link>
+                    ),
+                    headerShown: true,
+                    headerTitle: "Detalhes da Consulta",
+                    headerTitleAlign: "center",
+                    headerStyle: {
+                        backgroundColor: Colors[theme].background,
+                    },
+                    headerTitleStyle: { color: Colors[theme].text },
+                }}
+            />
+        </Stack>
     );
 }
