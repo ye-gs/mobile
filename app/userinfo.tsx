099613a54201500968f5145085310cf6ae351e15
EnricoGia <rico500000@gmail.com>
2024-05-04 19:19:39 -0300

    fix(userInfo): changing user information buttons to input

--- app/userinfo.tsx
+++ app/userinfo.tsx
@@ -1,49 +1,74 @@
 import { View } from "@/components/Themed";
 import { useUser } from "@/contexts/user";
-import { StyleSheet } from "react-native";
+import { KeyboardAvoidingView, StyleSheet } from "react-native";
 import { ProfileCard } from "@/components/ProfileCard";
 import { OptionButton } from "@/components/profile/OptionButton";
+import Colors from "@/constants/Colors";
+import { useTheme } from "@/contexts/theme";
 import { RFValue } from "react-native-responsive-fontsize";
 import { GenderSelect } from "@/components/profile/GenderSelect";
-
+import { TextInput } from "react-native-paper";
 export default function UserInfo() {
     const { user } = useUser();
+    const { theme } = useTheme();
+
     const styles = StyleSheet.create({
         container: {
             flex: 1,
             alignItems: "center",
             justifyContent: "center",
             gap: RFValue(10, 808),
+            backgroundColor: Colors[theme].background,
         },
         options: {
             height: "50%",
+            gap: RFValue(10, 808),
+        },
+        input: {
+            backgroundColor: Colors[theme].background,
         },
     });
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView style={styles.container} keyboardVerticalOffset={RFValue(120, 808)} behavior="position" enabled={true}>
             <ProfileCard
                 user={user}
                 imageSize={150}
                 paddingTop={0}
             ></ProfileCard>
             <View style={styles.options}>
-                <OptionButton
-                    text={"Nome: " + user?.displayName}
-                    borderBottomWidth={1}
-                    fontWeight="600"
-                ></OptionButton>
+                <TextInput
+                    label="Nome"
+                    defaultValue={user?.displayName!}
+                    style={styles.input}
+                    textColor={Colors[theme].text}
+                    outlineColor={Colors[theme].text}
+                    activeOutlineColor={Colors[theme].altTextColor}
+                    underlineColor={Colors[theme].text}
+                    activeUnderlineColor={Colors[theme].altTextColor}
+                ></TextInput>
+                <TextInput
+                    label="Email"
+                    defaultValue={user?.email!}
+                    style={styles.input}
+                    textColor={Colors[theme].text}
+                    outlineColor={Colors[theme].text}
+                    activeOutlineColor={Colors[theme].altTextColor}
+                    underlineColor={Colors[theme].text}
+                    activeUnderlineColor={Colors[theme].altTextColor}
+                ></TextInput>
+                <TextInput
+                    label="Data de Nascimento"
+                    defaultValue={user?.metadata.creationTime!}
+                    style={styles.input}
+                    textColor={Colors[theme].text}
+                    outlineColor={Colors[theme].text}
+                    activeOutlineColor={Colors[theme].altTextColor}
+                    underlineColor={Colors[theme].text}
+                    activeUnderlineColor={Colors[theme].altTextColor}
+                    placeholderTextColor={Colors[theme].altTextColor}
+                ></TextInput>
                 <GenderSelect />
-                <OptionButton
-                    text={"Email: " + user?.email}
-                    borderBottomWidth={1}
-                    fontWeight="600"
-                ></OptionButton>
-                <OptionButton
-                    text={"Data de nascimento: " + user?.metadata.creationTime}
-                    borderBottomWidth={1}
-                    fontWeight="600"
-                ></OptionButton>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
