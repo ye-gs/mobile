dd397bddf3267ea0ee09b420996a9021d5f215f2
EnricoGia <rico500000@gmail.com>
2024-05-05 17:05:00 -0300

    feature(userInfo): disabling input and adding datepicker

--- app/userinfo.tsx
+++ app/userinfo.tsx
@@ -2,16 +2,34 @@ import { View } from "@/components/Themed";
 import { useUser } from "@/contexts/user";
 import { KeyboardAvoidingView, StyleSheet } from "react-native";
 import { ProfileCard } from "@/components/ProfileCard";
-import { OptionButton } from "@/components/profile/OptionButton";
+import { Text } from "@/components/Themed";
+import { Button } from "react-native-paper";
 import Colors from "@/constants/Colors";
 import { useTheme } from "@/contexts/theme";
 import { RFValue } from "react-native-responsive-fontsize";
 import { GenderSelect } from "@/components/profile/GenderSelect";
 import { TextInput } from "react-native-paper";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { useState } from "react";
 export default function UserInfo() {
     const { user } = useUser();
     const { theme } = useTheme();
+    const [datetime, setDatetime] = useState(
+        new Date(user?.metadata.creationTime!) || new Date()
+    );
+    const [datePickerVisible, setDatePickerVisible] = useState(false);
+    const showDatePicker = () => {
+        setDatePickerVisible(true);
+    };
 
+    const hideDatePicker = () => {
+        setDatePickerVisible(false);
+    };
+
+    const handleConfirm = (date: Date) => {
+        setDatetime(date);
+        hideDatePicker();
+    };
     const styles = StyleSheet.create({
         container: {
             flex: 1,
@@ -27,6 +45,10 @@ export default function UserInfo() {
         input: {
             backgroundColor: Colors[theme].background,
         },
+        datepickerButton:{
+            borderWidth: 1,
+            borderColor: "gray",
+        }
     });
     return (
         <KeyboardAvoidingView style={styles.container} keyboardVerticalOffset={RFValue(120, 808)} behavior="position" enabled={true}>
@@ -46,6 +68,7 @@ export default function UserInfo() {
                     underlineColor={Colors[theme].text}
                     activeUnderlineColor={Colors[theme].altTextColor}
                 ></TextInput>
+               
                 <TextInput
                     label="Email"
                     defaultValue={user?.email!}
@@ -55,19 +78,25 @@ export default function UserInfo() {
                     activeOutlineColor={Colors[theme].altTextColor}
                     underlineColor={Colors[theme].text}
                     activeUnderlineColor={Colors[theme].altTextColor}
+                    disabled={true}
                 ></TextInput>
-                <TextInput
-                    label="Data de Nascimento"
-                    defaultValue={user?.metadata.creationTime!}
-                    style={styles.input}
-                    textColor={Colors[theme].text}
-                    outlineColor={Colors[theme].text}
-                    activeOutlineColor={Colors[theme].altTextColor}
-                    underlineColor={Colors[theme].text}
-                    activeUnderlineColor={Colors[theme].altTextColor}
-                    placeholderTextColor={Colors[theme].altTextColor}
-                ></TextInput>
-                <GenderSelect />
+                 <Button onPress={showDatePicker} style={styles.datepickerButton}>
+                    <Text style={{ color: Colors[theme].text }}>
+                        Data de nascimento {" "}
+                        {datetime
+                            ? datetime.toLocaleString("pt-Br")
+                            : "Nenhuma data selecionada"}
+                    </Text>
+                </Button>
+                <DateTimePickerModal
+                    date={datetime ? new Date(datetime) : undefined}
+                    isVisible={datePickerVisible}
+                    mode="datetime"
+                    onConfirm={handleConfirm}
+                    display="default"
+                    onCancel={hideDatePicker}
+                />
+                <GenderSelect/>
             </View>
         </KeyboardAvoidingView>
     );
