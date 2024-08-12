1c9827b343da397adf71468ecdfbc1ab8c84a94a
EnricoGia <rico500000@gmail.com>
2024-08-12 17:23:15 -0300

    tests(document): document picker

--- app/(tabs)/exams.tsx
+++ app/(tabs)/exams.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text } from "@/components/Themed";
-import { StyleSheet } from "react-native";
+import { View, Text, } from "@/components/Themed";
+import { StyleSheet, Button } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+
+const pickDocument = async () => {
+    let result = await DocumentPicker.getDocumentAsync({ 
+      type: "*/*", // You can specify the type of document you want to pick (e.g., "application/pdf" for PDFs)
+      copyToCacheDirectory: true,
+      multiple: false, // Set this to true if you want to allow picking multiple files
+    });
+      console.log("File URI:", result);
+  };
 
 const Exams = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Hello, React Native!</Text>
+            <Button title="Inserir documento" onPress={pickDocument}></Button>
         </View>
     );
 };
