7273bab5c464997acc684966a990c366327d25af
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-08-19 17:08:27 -0300

    feat: add button to send file to functions api

--- app/(tabs)/exams.tsx
+++ app/(tabs)/exams.tsx
@@ -1,22 +1,81 @@
 import React from "react";
-import { View, Text, } from "@/components/Themed";
-import { StyleSheet, Button } from "react-native";
+import { View, Text } from "@/components/Themed";
+import { StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 import * as DocumentPicker from "expo-document-picker";
-
-const pickDocument = async () => {
-    let result = await DocumentPicker.getDocumentAsync({ 
-      type: "*/*", // You can specify the type of document you want to pick (e.g., "application/pdf" for PDFs)
-      copyToCacheDirectory: true,
-      multiple: false, // Set this to true if you want to allow picking multiple files
-    });
-      console.log("File URI:", result);
-  };
+import * as FileSystem from "expo-file-system";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Exams = () => {
+    const [selectedFileUri, setSelectedFileUri] = React.useState<string | null>(
+        null
+    );
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [selectedFileName, setSelectedFileName] = React.useState<
+        string | null
+    >(null);
+    const pickDocument = async () => {
+        const result = await DocumentPicker.getDocumentAsync({
+            type: "application/pdf",
+        });
+        if (!result.canceled) {
+            setSelectedFileUri(result.assets[0]?.uri);
+            setSelectedFileName(result.assets[0]?.name);
+        }
+    };
+
+    const sendData = async () => {
+        setLoading(true);
+        if (!selectedFileUri) {
+            return;
+        }
+        try {
+            // Retrieve the token from AsyncStorage
+            const token = await AsyncStorage.getItem("@app:session");
+
+            const uploadTask = FileSystem.createUploadTask(
+                "https://send-exam-uoy6brlebq-rj.a.run.app/",
+                selectedFileUri,
+                {
+                    headers: {
+                        "Content-Type": "application/pdf",
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+
+            const response = await uploadTask.uploadAsync();
+            if (response?.status !== 200) {
+                setLoading(false);
+
+                alert("Erro ao enviar arquivo " + response?.body);
+            }
+            setLoading(false);
+            alert("Arquivo enviado com sucesso");
+        } catch (e) {
+            setLoading(false);
+            const error = e as Error;
+            console.log(error);
+            console.log(error.toString());
+            alert("Erro ao enviar arquivo " + error.message);
+            console.log(error.message);
+        }
+    };
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Hello, React Native!</Text>
-            <Button title="Inserir documento" onPress={pickDocument}></Button>
+            <Button onPress={pickDocument} mode="contained">
+                Inserir documento
+            </Button>
+            {selectedFileUri && (
+                <Button loading={loading} mode="contained" onPress={sendData}>
+                    Enviar
+                </Button>
+            )}
+            {selectedFileName && (
+                <View>
+                    <Text> Arquivo selecionado: {selectedFileName}</Text>
+                </View>
+            )}
         </View>
     );
 };
