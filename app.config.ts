85d2c7a29914ae101eeda45186fa67702ed55878
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-08-19 21:25:25 -0300

    fix: env vars

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,15 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => {
     const googleServicesFile = process.env.GOOGLE_SERVICES_FILE;
-    const firebaseApiKey = process.env.FIREBASE_API_KEY || "";
-    const authDomain = process.env.FIREBASE_AUTH_DOMAIN || "";
-    const projectId = process.env.FIREBASE_PROJECT_ID || "";
-    const storageBucket = process.env.FIREBASE_STORAGE_BUCKET || "";
-    const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID || "";
-    const appId = process.env.FIREBASE_APP_ID || "";
-    const measurementId = process.env.FIREBASE_MEASUREMENT_ID || "";
-    const webClientId = process.env.FIREBASE_WEB_CLIENT_ID || "";
+    const firebaseApiKey = process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "";
+    const authDomain = process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "";
+    const projectId = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "";
+    const storageBucket = process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "";
+    const messagingSenderId =
+        process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "";
+    const appId = process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "";
+    const measurementId = process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || "";
+    const webClientId = process.env.EXPO_PUBLIC_FIREBASE_WEB_CLIENT_ID || "";
 
     return {
         ...config,
