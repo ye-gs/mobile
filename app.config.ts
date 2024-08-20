7f3769711695bcb2bc1258391c7a84043ee3b655
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-08-19 21:35:44 -0300

    throw error if env vars aren't defined

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,42 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => {
     const googleServicesFile = process.env.GOOGLE_SERVICES_FILE;
-    const firebaseApiKey = process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "";
-    const authDomain = process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "";
-    const projectId = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "";
-    const storageBucket = process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "";
+    const firebaseApiKey = process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+    const authDomain = process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN;
+    const projectId = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID;
+    const storageBucket = process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET;
     const messagingSenderId =
-        process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "";
-    const appId = process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "";
-    const measurementId = process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || "";
-    const webClientId = process.env.EXPO_PUBLIC_FIREBASE_WEB_CLIENT_ID || "";
+        process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+    const appId = process.env.EXPO_PUBLIC_FIREBASE_APP_ID;
+    const measurementId = process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID;
+    const webClientId = process.env.EXPO_PUBLIC_FIREBASE_WEB_CLIENT_ID;
+    if (!googleServicesFile) {
+        throw new Error("GOOGLE_SERVICES_FILE must be defined");
+    }
+    if (!firebaseApiKey) {
+        throw new Error("FIREBASE_API_KEY must be defined");
+    }
+    if (!authDomain) {
+        throw new Error("FIREBASE_AUTH_DOMAIN must be defined");
+    }
+    if (!projectId) {
+        throw new Error("FIREBASE_PROJECT_ID must be defined");
+    }
+    if (!storageBucket) {
+        throw new Error("FIREBASE_STORAGE_BUCKET must be defined");
+    }
+    if (!messagingSenderId) {
+        throw new Error("FIREBASE_MESSAGING_SENDER_ID must be defined");
+    }
+    if (!appId) {
+        throw new Error("FIREBASE_APP_ID must be defined");
+    }
+    if (!measurementId) {
+        throw new Error("FIREBASE_MEASUREMENT_ID must be defined");
+    }
+    if (!webClientId) {
+        throw new Error("FIREBASE_WEB_CLIENT_ID must be defined");
+    }
 
     return {
         ...config,
