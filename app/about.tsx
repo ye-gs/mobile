96243c870c07951702d67826d8af38c1fe8af054
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-09-28 15:17:43 -0300

    fix: change about to scroll view

--- app/about.tsx
+++ app/about.tsx
@@ -50,24 +50,51 @@ export default function UserInfo() {
         },
     });
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <StatusBar style="auto" />
-            <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}>
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1, alignItems: "center" }}
+            >
                 <View style={styles.main}>
                     <Text style={styles.header}>YE Gestão de Saúde</Text>
                     <View style={styles.mainContent}>
                         <Text style={styles.mainText}>
-                            A YE Gestão de Saúde é uma empresa inovadora no setor de tecnologia para a saúde, dedicada ao desenvolvimento de soluções inteligentes que tornam o gerenciamento da saúde pessoal mais eficiente e acessível. Nosso principal projeto é um aplicativo de gestão de saúde que utiliza inteligência artificial para fornecer lembretes personalizados e dicas médicas, ajudando os usuários a manterem um controle rigoroso sobre sua saúde e bem-estar.
+                            A YE Gestão de Saúde é uma empresa inovadora no
+                            setor de tecnologia para a saúde, dedicada ao
+                            desenvolvimento de soluções inteligentes que tornam
+                            o gerenciamento da saúde pessoal mais eficiente e
+                            acessível. Nosso principal projeto é um aplicativo
+                            de gestão de saúde que utiliza inteligência
+                            artificial para fornecer lembretes personalizados e
+                            dicas médicas, ajudando os usuários a manterem um
+                            controle rigoroso sobre sua saúde e bem-estar.
                             {"\n\n"}
-                            Nosso aplicativo é projetado para ser um assistente pessoal de saúde, que lembra os usuários de consultas médicas, exames e horários de medicação. Além disso, oferece sugestões e orientações personalizadas com base nas necessidades e histórico de saúde de cada indivíduo. Através da inteligência artificial, buscamos tornar a gestão da saúde mais intuitiva e proativa, promovendo a prevenção e facilitando a adesão a hábitos saudáveis.
+                            Nosso aplicativo é projetado para ser um assistente
+                            pessoal de saúde, que lembra os usuários de
+                            consultas médicas, exames e horários de medicação.
+                            Além disso, oferece sugestões e orientações
+                            personalizadas com base nas necessidades e histórico
+                            de saúde de cada indivíduo. Através da inteligência
+                            artificial, buscamos tornar a gestão da saúde mais
+                            intuitiva e proativa, promovendo a prevenção e
+                            facilitando a adesão a hábitos saudáveis.
                             {"\n\n"}
-                            Na YE Gestão de Saúde, acreditamos que a tecnologia pode transformar a forma como cuidamos da nossa saúde, tornando o acesso a informações médicas e a manutenção de uma rotina saudável mais simples e eficaz. Estamos comprometidos em oferecer um aplicativo que não apenas auxilia no dia a dia, mas também contribui para uma vida mais saudável e equilibrada.
+                            Na YE Gestão de Saúde, acreditamos que a tecnologia
+                            pode transformar a forma como cuidamos da nossa
+                            saúde, tornando o acesso a informações médicas e a
+                            manutenção de uma rotina saudável mais simples e
+                            eficaz. Estamos comprometidos em oferecer um
+                            aplicativo que não apenas auxilia no dia a dia, mas
+                            também contribui para uma vida mais saudável e
+                            equilibrada.
                         </Text>
-                        
-                        <Text style={styles.slogan}>Sua saúde em suas mãos!</Text>
+
+                        <Text style={styles.slogan}>
+                            Sua saúde em suas mãos!
+                        </Text>
                     </View>
                 </View>
             </ScrollView>
-        </View>
+        </ScrollView>
     );
 }
