27b17cb04a8f2e3a38fa0caf3be0153f7db62948
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-10-04 12:25:18 -0300

    deps: add import react

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,16 +2,21 @@ import { Link, Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 import { Text, View } from "@/components/Themed";
 import { GeneralStyles } from "@/constants/Styles";
+import React from "react";
 
 export default function NotFoundScreen() {
     return (
         <>
             <Stack.Screen options={{ title: "Oops!" }} />
             <View style={GeneralStyles().container5}>
-                <Text style={GeneralStyles().title1}>This screen doesn't exist.</Text>
+                <Text style={GeneralStyles().title1}>
+                    This screen doesn't exist.
+                </Text>
 
                 <Link href="/" style={GeneralStyles().link1}>
-                    <Text style={GeneralStyles().linkText1}>Go to home screen!</Text>
+                    <Text style={GeneralStyles().linkText1}>
+                        Go to home screen!
+                    </Text>
                 </Link>
             </View>
         </>
