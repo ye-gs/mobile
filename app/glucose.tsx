70721cea876017b8f11e95bc1f9d4969d0971f8e
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2024-10-07 13:54:52 -0300

    feat: add crud for pressure and glucose measures

--- app/glucose.tsx
+++ app/glucose.tsx
@@ -1,30 +1,4 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
 import GlucoseMeasures from "@/components/GlucoseMeasures";
-import { useTheme } from "@/contexts/theme";
-import Colors from "@/constants/Colors";
-import { GeneralStyles } from "@/constants/Styles";
-
-const GlucosePage = () => {
-    const { theme } = useTheme();
-    return (
-        <View
-            style={[
-                GeneralStyles().container4,
-                //{ backgroundColor: Colors[theme].background },
-            ]}
-        >
-            <GlucoseMeasures />
-        </View>
-    );
-};
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         alignItems: "center",
-//         justifyContent: "center",
-//     },
-// });
-
-export default GlucosePage;
+export default function Glucose() {
+    return <GlucoseMeasures></GlucoseMeasures>;
+}
